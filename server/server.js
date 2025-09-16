b32b0d842c4157b843c7a03d6294ac53e7501520
Sachin VS <vs1sachi12@gmail.com>
2025-09-16 15:45:11 +0530

    first commit756

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const winston = require("winston");
-const connectdb = require("./db/env");
+const connectdb = require("./env/db");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const cookiparser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const fileupload = require("express-fileupload");
 const dotenv = require("dotenv");
-
-const app = express()
