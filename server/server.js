e6818ce45dc3d1948e3bc40b83dfa9697166852e
Sachin VS <vs1sachi12@gmail.com>
2025-09-16 16:49:33 +0530

    first commit761

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
-import express from 'express';
+const winston = require("winston");
+const connectDB = require("./env/db");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie -parser");
+const fileupload = require("express-fileupload");
+const dotenv = require("dotenv");
+const errorHandler = require("./middlewares/error");
+const authRoute = require("./routers/auth-router");
+const supportmailRoute = require("./routes/suportmail-routes");
+const mqttroutes = require("routes/mqttRoutes");
+const bckupdbRoute = require("./routers/backupdb-route");
 
+//Load environments variables
+dotenv.config({ path: "./.env" });
+
+// Intialize Express
 const app = express();
 
-app.post("/", (req, res) =>{
-   res.send("hello samith r gowda");
-});
 
-app.listen(3000, ()=>{
-   console.log("server is running on port 3000");
-});
