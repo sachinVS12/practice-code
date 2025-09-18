947d01256978f1631dca12fc871a6b11cf715b2e
Sachin VS <vs1sachi12@gmail.com>
2025-09-18 16:08:38 +0530

    first commit769

--- server/server.js
+++ server/server.js
@@ -1,45 +1,32 @@
 const winston = require("winston");
-const connectdb = require("./env/db");
+const connectedb = require("./env/db");
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
 const cookieparser = require("cookie-parser");
+const morgan = require("morgan");
 const fileupload = require("express-fileupload");
 const dotenv = require("dotenv");
-const errorHnadler = require("./middleware/error");
-const authroutes = require("./routers/auth-routes");
-const mqttroutes = require(".routers/mqttroutes");
-const suppotreamilroutes = require(".routers/supportemail-routes");
-const backupdbroutes = require("./routers/backuproutes");
+const errorHandler = require("./middleware/error");
+const authroutes = require("./routes/auth-routes");
+const mqttroutes = require("./routers/mqttroutes");
+const supportmailroutes = require("./routers/supportmailroutes");
+const backupdbroutes = require("./routers/backudbproutes");
 
-//load envrionment variable
-dotenv.config({ path: "./env./"});
+//load environment variable
+dotenv.config({ path:"./.env"});
 
-//initialize
-const app = express();
+//initialize express
+const app = require(express);
 
 //Logger configuration
-const Logger = winston.createlogger({
-   level: "info",
+const Logger = winston.createLogger({
+   level:"info",
    format: winston.format.combine(
-      winston.format.timestamps(),
+      winston.format.timstamps(),
       winston.format.json()
    ),
    tarnsports: [
-      new winston.tarnsports.File({ filename: "error,log", level:"error"}),
-      new winston.tarnsports.File({ filename: "combined.log"}),
+      new winston.tarnsports.File({filename: "error.log", level:"error"}),
+      new winston.tarnsports.File({filename: "combined.log"}),
    ],
 });
-
-//middleware
-app.use(express.json());
-app.use(fileupload());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-   origin: "http://13.232.224.195:3000",
-   methods: ["GET","POST", "PUT", "DELETE", "PATCH"],
-   exposedHeaders: ['Content-Length', 'Content-Dispostion'],
-   maxage: 86400
-}));
-app.use(cookieparser());
-
