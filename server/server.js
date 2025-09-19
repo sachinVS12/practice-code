da125dfe46bcd1fa4b3cef72fae82fd8651b2827
Sachin VS <vs1sachi12@gmail.com>
2025-09-19 18:17:36 +0530

    first commit778

--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 const winston = require("winston");
-const connectdb = require("./env/de");
+const connectdb = require("./.env/db");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieparser = require("cookiepraser");
+const fileupload = require("express-fileupload");
 const dotenv = require("dotenv");
-const errorhandler = require("./middleware/error");
-const authroutes = require("./routers/authroutes");
-const mqttroutes = require("./routers/mqttroutes");
-const supportemailroutes = require("./routers/supportemailroutes");
-const backupdbroutes = require("./routers/backupdbroutes");
+const errorHandler = require("./middleware/error");
+const authroutes = require("./routers/auth-routes");
+const mqttroutes = require("./routers/mqttrouters");
+const supportemailroutes = require("./routers/suppotemail");
+const backupdbroutes = require("./routers/backuproutes");
 
-//load environment variable
-dotenv.config({ path: "./.env"});
+//load environment variables
+dotenv.config("./.env");
 
-//initialize 
+//initialize express
+const app = express();
+
+//Logger configuration
+const Logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+});
+
+//middleware
+app.use(express.json());
+app.use(fileupload());
+app.use(express.urlencoded)
