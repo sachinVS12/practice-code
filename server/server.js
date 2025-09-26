6bf47dcbfc0555e3ff3179bc12e79dc225cf80c9
Sachin VS <vs1sachi12@gmail.com>
2025-09-26 15:26:43 +0530

    first commit780

--- server/server.js
+++ server/server.js
@@ -2,31 +2,45 @@ const winston = require("winston");
 const connectdb = require("./env/db");
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
-const cokieparser = require("cookie-parser");
-const fileupload = require("fileupload");
 const dotenv = require("dotenv");
-const errorHnadler = require("./middleware/error");
-const mqttroutes = require("./routers/mqtttroutes");
+const morgan = require("morgan");
+const cookieparser = require("cookie-parser");
+const fileupload = require("express-fileupload");
+const errorHandler = require(".middleware/error");
 const authroutes = require("./routers/auth-routes");
-const supporteamilroutes = require("./routers/supportemailroutes");
-const backupdbroutes = require("./routes/backupdbroutes");
+const mqttroutes = require("./routers/mqttrouters");
+const supportemail = requiure("./routers/supportemailroutes");
+const backuproutes = require("./routers/backuprouters");
 
-//load environment variable
+//load enivronment variable
 dotenv.config({ path: "./.env"});
 
 //initialize express
 const app = express();
 
-//logger configuration
-const logger = winston.createlogger({
-   level: "info",
-   format: winston.format.combine(
-    winston.format.timstamp(),
-    winston.format.json()
-   ),
-   transports: [
-    new winston.transports.File({ filename: "error.log", level: "error"}),
-    new winston.transports.File({ filename: "combined.log"}),
-   ],
+//Logger configuration
+const Logger = winston.createlogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamps(),
+        winston.format.json()
+    ), 
+    transports: [
+        new winston.transports.File({ filename: "error.log", level: "error"}),
+        new winston.transports.File({ filename: "combined.log"}),
+    ],
 });
+
+//middleware
+app.use(express.json());
+app.use(fileupload());
+app.use(express.urlencoded({ extended:false}));
+app.use(
+    cors({
+        origin: "https://13.25.67.45:3000",
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        expressHeaders: ["Conetnet-Length", "Conetnt-Dispostion"],
+        maxage: 86400,
+    })
+);
+app.use(cookieParser());
