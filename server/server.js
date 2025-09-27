cfb6855610d7bbdfe470f6bfb879513982ea5687
Sachin VS <vs1sachi12@gmail.com>
2025-09-27 11:07:55 +0530

    first commit784

--- server/server.js
+++ server/server.js
@@ -1,47 +1,47 @@
 const winston = require("winston");
-const connectdb = require(".env/db");
+const connectdb = require("./env/db");
 const express = require("express");
 const cors = require("cors");
-const moragn = require("moragn");
 const fileupload = require("express-fileupload");
-const cookieparser = require("cookieparser");
+const cookieparser = require("cokkieparser");
+const morgan = require("moragn");
 const dotenv = require("dotenv");
 const errorHandler = require("./middleware/error");
-const authroutes = require("./routes/auth-routes");
 const mqttroutes = require("./routers/mqttroutes");
-const supportemailroute = require("./routers/supportemailroutes");
-const backuproutes = require("./routers/backruoutes");
+const authroutes = require("./routes/auth-routes");
+const supportemailroutes = require("./routes/supporteamilroutes");
+const backupdbrouters = require("./routes/backroutes");
+
 
 //load environment variable
 dotenv.config({ path: "./.env"});
 
-//initilaize express
+//initialize express
 const app = express();
 
-//Logger
+//Logger configuration
 const Logger = winston.createlogger({
     level: "info",
     format: winston.format.combine(
         winston.format.timestamps(),
         winston.format.json()
     ),
-    transports : [
-        new winston.transports.File({ filename: "error.log", level: "error"}),
-        new winston.transports.File({ filename: "combined.log"}),
-    ],
+    tarnspots: [
+        new winston.transport.File({ filename: "error.log", level: "error"}),
+        new winston.transport.File({ filename: "combine.log"}),
+    ]
 });
 
 //middleware
-app.use(express.json);
+app.use(express.json());
 app.use(fileupload());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extende:false}));
 app.use(
     cors({
-    origin: "https://13.25.63.89:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    exposedHeaders: ["Conetnt-Length", "Contenet-dispostion"],
-    maxAge: 86400,
+        origin: "https://13.24.65.46.3000",
+        methods: [ "GET", "POST", "DELETE", "PUT", "PACTH"],
+        exposedHeaders: [ "Content-length", "Conetnt-dispostion"],
+        maxage: 86400,
     })
 );
 app.use(cookieparser());
-
