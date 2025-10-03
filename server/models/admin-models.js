9ffbecbf325c0d5398578e59543abb8cf5d6c527
Sachin VS <vs1sachi12@gmail.com>
2025-10-03 16:13:51 +0530

    first commit796

---
+++ server/models/admin-models.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require('crypto');
