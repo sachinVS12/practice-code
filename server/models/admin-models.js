8025821c33f98c2f1e0f75b72840321293a9a467
Sachin VS <vs1sachi12@gmail.com>
2025-10-03 17:42:56 +0530

    first commit801

--- server/models/admin-models.js
+++ server/models/admin-models.js
@@ -1,29 +1,26 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const { emitWarning } = require("process");
 const { match } = require("assert");
 const { type } = require("os");
 
 const adminSchema = new mongoose.Schema(
     {
         name: {
-            type: "String",
+            type: String,
             required: [true, "name is required"],
         },
         email: {
-            type: "String",
+            type: string,
             required: [true, "email is required"],
             unique: true,
-            match: [/.+\@.+\..+/, "please enter the valide email"],
+            match: [/..\@.+\..+/, "please enter the valide email"],
         },
         password: {
-            type: "string",
+            type: string,
             select: false,
             required: [true, "password is required"],
         },
-        resetpasswordtoken: String,
-        resetpasswordexperire: Date,
     }
 )
