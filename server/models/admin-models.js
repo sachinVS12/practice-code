a4e0c3642f9595236d4f5469f8c5cc73ee392837
Sachin VS <vs1sachi12@gmail.com>
2025-10-07 15:37:05 +0530

    first commit813

--- server/models/admin-models.js
+++ server/models/admin-models.js
@@ -2,37 +2,35 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const { match } = require("assert");
-const { type } = require("os");
 const { timeStamp } = require("console");
 
-const adminSchema = new mongooseSchema(
+const adminschema = new mongooseschema(
     {
         name: {
             type: String,
-            required: [true, "name is required"]
+            required: [true, "name is required"],
         },
         email: {
-            type: String,
-            required: [true, "email is required"],
-            unique: true,
-            match: [/.+\@.+\..+/, "please enter valid email id"]
+             type:String,
+             required: [true, "email is required"],
+             unique: true,
+             match: [/.+\@.+\..+/, "please enter valid email id"],
         },
         password: {
             type: String,
             select: false,
-            required: [true, "password is required"]
+            required: [true, "password is required"],
         },
         resetpasswordtoken: String,
-        resetpasswordExpire: Date,
-        role: {
+        resetpasswordexpeire: Date,
+        role:{
             type: String,
             default: "admin",
         },
     },
     {
-      timeStamp: true,
-      toJson: { virtual: true},
-      toObject: { virtual: true}
-    }
-);
+        timeStamp:true,
+         toJson : { virtuals: true},
+        toObject : { virtuals: true},
+    }       
+)
