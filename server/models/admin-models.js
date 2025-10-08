6a8f5221e7b3f63668a3466bb81782ebab67a5d3
Sachin VS <vs1sachi12@gmail.com>
2025-10-08 16:17:50 +0530

    first commit818

--- server/models/admin-models.js
+++ server/models/admin-models.js
@@ -44,3 +44,17 @@ adminschema.pre("save", async function(next) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
