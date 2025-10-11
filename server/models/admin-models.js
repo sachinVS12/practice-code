a16d51d9e4d6c4600b52ccd7c322b40a1b3c0ee7
Sachin VS <vs1sachi12@gmail.com>
2025-10-11 10:45:26 +0530

    first commit824

--- server/models/admin-models.js
+++ server/models/admin-models.js
@@ -1,39 +1,23 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const crypto = require("crypto");
-const { match } = require("assert");
-const { type } = require("os");
 
 const adminschema = new mongooseschema(
     {
         name: {
             type: String,
-            required: [true, "name is required"],
+            required: [true, "Name is required"],
         },
         email: {
             type: String,
-            required: [true, "email is required"],
-            unique: true,
-            match: [/.+\@.+\..+/, "enter valid email id"],
+            require: [true, "email is required"],
+            unique:true,
+            match: [/.+\@.+\..+/, "Enter the valid email id"],
         },
         password: {
             type: String,
             select: false,
-            required: [true, "password is require"],
-        },
-        role: {
-            type: String,
-            default: "Admin",
+            required:[true, "passsword is required"]
         }
-    },
-);
-
-//pre-save middleware to hash the password to before store the database
-adminschema.pre("save", async function (next){
-    if(!this.ismodefied("password")) {
-        return next();
     }
-    const salt = await bcrypt.gensalt(10);
-    this.password = await bcrypt.hash(this.paasword, salt);
-});
+)
