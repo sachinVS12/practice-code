eb7bd3093dfafa9e9e8a8554f42aa3ae514dc01d
Sachin VS <vs1sachi12@gmail.com>
2025-10-11 11:00:02 +0530

    first commit825

--- server/models/admin-models.js
+++ server/models/admin-models.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//Define the user schema
 const adminschema = new mongooseschema(
     {
         name: {
@@ -18,6 +19,34 @@ const adminschema = new mongooseschema(
             type: String,
             select: false,
             required:[true, "passsword is required"]
-        }
+        },
+        role: {
+            type: String,
+            defalut: "employee",
+        },
+    },
+    {
+        timestamps: true,
     }
-)
+);
+
+// pre-save middleware to hash the password befor store database
+adminschema.pre("save", async function (next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+    const salt = await bcrypt.gensalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+ next();   
+});
+
+// method to generate the jwt token for the loggedin or signedupo users
+adminschema.methods.getToken = function (){
+    return jwt.sign(
+        { id: this._id, name: this.name, email: this.email, role: this.role},
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "3d",
+        }
+    );
+};
