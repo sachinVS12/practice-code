04535a2a642b795793598836008e0b8a2a4f31ec
Sachin VS <vs1sachi12@gmail.com>
2025-10-15 17:27:06 +0530

    first commit836

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const winston = require("winston");
-const connectdb = require("./env/db");
+const connectdb = require("./db/env");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const fileupload = require("express-fileupload");
 const cookieparser = require("cookieparser");
 const dotenv = require("dotenv");
-const errorHnadler = require("./middleware/error");
+const errorHandler = require("./middleware/error");
 const authrouters = require("./routers/authrouters");
 const mqttrouters = require("./routers/mqttrouters");
 const supportemailrouters = require("./routers/supportemailrouters");
-const backupdbrouters = require("./routers/backupdbrouters");
+const backupdbrouters = require("./routers/backuprouters");
 
-//load environemnt vaiable
-dotenv.config({path: "./.env"});
+//load environment variable
+dotenv.config({ path: "./.env"});
 
 //initialize express
 const app = express();
@@ -22,22 +22,22 @@ const app = express();
 const logger = winston.createlogger({
     level: "info",
     format: winston.format.combine(
-        winston.format.timestamp(),
+        winston.format.timestamps(),
         winston.format.json()
     ),
     transports: [
-        winston.transports.File({ filename: "error.log", level:"error"}),
-        winston.transports.File({ filename: "combine"},)
+        winston.transports.File({ fielname: "error.log", level: "error"}),
+            winston.transports.File({ fielname: "combine"}),
     ],
 });
 
-//midleware
+//middleware
 app.use(express.json());
 app.use(fileupload());
 app.use(express.urlencoded({ extended: false}));
 app.use(
     cors({
-        origin: "http://12.23.45.67:3000",
+        origin: "https://12.23.45.67:3000",
         methods: ["GET", "PUT", "DELETE", "PATCH"],
         exposedHeaders: ["Content-Length", "Content-dispostion"],
         maxAge: 86400
@@ -45,32 +45,4 @@ app.use(
 );
 app.use(cookieparser());
 
-//inrease request to timeout and enables chunkked responses
-app.use((req, res, next)=>{
-    req.setTimeout(6000); //10 minutes timeout
-    res.setTimeout(6000); //10 minutes timeout
-    res.flush = res.flush || (()=>{}); // flush is avialble
-    logger.info(`Requested to : set ${req.url}`,{
-        method: req.method,
-        body: req.body,
-    });
-    next();
-});
-
-//Routers
-app.use("api/v1/auth", authroutersrouters);
-app.use("api/v1/mqtt", mqttrouters);
-app.use('api/v1/supportemail', supportemailrouters);
-app.use("api/v1/backupdb", backupdbrouters);
-
-//errorhandler
-app.use(errorHnadler);
-
-//database connection
-connectdb();
-
-//start the server
-const port = process.env.PORT || 5000;
-app.listen(port, "0.0.0.0", ()=>{
-    logger.info(`server is running on port: ${port}`);
-});
+//increase request timeout and enables chunnkked variable
